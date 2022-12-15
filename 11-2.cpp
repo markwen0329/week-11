084a14f331bb882d5c186917b6a92f5894068d7a
markwen0329 <114090782+markwen0329@users.noreply.github.com>
2022-12-15 19:10:53 +0800

    Create 11-2.cpp

---
+++ 11-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int data[5];
+    printf("Enter elements");
+    for (int i = 0; i < 5; ++i)
+        scanf("%d", data + i);
+    printf("You entered: \n");
+    for (int i = 0; i < 5; ++i)
+        printf("%d\n", *(data + i));    
+    return 0;
+}
